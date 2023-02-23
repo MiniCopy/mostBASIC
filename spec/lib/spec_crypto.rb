490da0c298bd27a3d300b54309f6f18dc50837a3
MiniCopy <119115015+MiniCopy@users.noreply.github.com>
2023-02-23 22:41:39 +0000

    * Cleanup

--- spec/lib/spec_crypto.rb
+++ spec/lib/spec_crypto.rb
@@ -30,4 +30,20 @@ subject {test_solver}
     test_solver.pop_dict[6].values.length.should be > 1100
     test_solver.dicts[6].values.length.should be > 1100
     test_solver.name_dict[6].values.length.should be > 100
-    te
+    test_solver.dict_1k[6].values.length.should be > 20
+  end
+
+  it "should create A list of puzzle objects" do
+    test_solver.puzzle_list = test_solver.conform_puzzles(test_root)
+    test_solver.puzzle_list[0].should be_an_instance_of(Puzzle)
+  end
+
+  it "should create puzzle objects with Crypto/Author/Date attribs" do
+    test_solver.puzzle_list = test_solver.conform_puzzles(test_root)
+    test_solver.puzzle_list[0].publ_date.should be_true
+    test_solver.puzzle_list[0].author.should be_true
+    test_solver.puzzle_list[0].crypto.should be_true
+  end
+
+  it 'should split the string at the /[.?!]"* - / point' do
+    a, b = test_solver.seperate_author("UWDC W FXWYFC! WI
